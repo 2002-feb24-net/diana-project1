ce4db23dea74153751a38340523dbacdc1f31899
Diana Lim <dianalim@Dianas-MacBook-Pro.local>
2020-04-08 19:43:19 -0500

    Can display order history

---
+++ LimsGarden/LimsGarden.Web/Models/OrderDetailsViewModel.cs
@@ -0,0 +1,10 @@
+using LimsGarden.DataAccess.Model;
+using System.Collections.Generic;
+
+namespace LimsGarden.Web.Models
+{
+    public class OrderDetailsViewModel
+    {
+        public List<OrderDetails> orders { get; set;}
+    }
+}
